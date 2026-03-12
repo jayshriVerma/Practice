fddb8b8d9e944b09dc4b34bacc748d5aafc87990
jayshriVerma <jayshriyog@gmail.com>
2026-03-12 20:37:37 +0000

    typescript

--- dynamic_programming/word_break.py
+++ dynamic_programming/word_break.py
@@ -32,3 +32,20 @@ def word_break(s, wordDict):
                 dp[j+1] = True
     return dp[-1]
 # Time Complexity: O(n³) because of substring check substring check → n
+
+function wordBreak(s: string, wordDict: string[]): boolean {
+    const wordSet = new Set(wordDict);
+
+    const dp: boolean[] = new Array(s.length + 1).fill(false);
+    dp[0] = true;
+
+    for(let i =1; i <= s.length; i++) {
+        for(let j = 0; j < i; j++) {
+            if (dp[j] && wordSet.has(s.substring(j,i))) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp[s.length];
+}
